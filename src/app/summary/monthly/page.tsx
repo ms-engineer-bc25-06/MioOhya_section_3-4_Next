90014c9c6bb1d5ce07ccd3fc3d7407eca4ab0757
Miocoji <your_email@example.com>
2025-06-24 21:44:33 +0900

    update: path of SWR

--- src/app/summary/monthly/page.tsx
+++ src/app/summary/monthly/page.tsx
@@ -10,7 +10,7 @@ function MonthlySummary() {
   const [selectedMonth, setSelectedMonth] = useState(new Date().toISOString().slice(0, 7)); // YYYY-MM形式
 
   const { data: allExpenses, error, isLoading } = useSWR<Expense[]>(
-    'http://localhost:3002/expenses',
+    'http://localhost:4000/expenses',
     fetcher
   );
 
